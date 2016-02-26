407e02da1be8c771617270eb67a4a7f2bd05ce86
dongja94 <dongja94@gmail.com>
2016-02-26 09:25:42 +0900

    Geocoder

--- app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,17 +17,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager mLM;
     TextView locationView;
     String mProvider = LocationManager.GPS_PROVIDER;
+
+    ListView listView;
+    ArrayAdapter<Address> mAdapter;
+    EditText keywordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +59,34 @@ public class MainActivity extends AppCompatActivity {
         });
         locationView = (TextView) findViewById(R.id.text_location);
         mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        listView = (ListView) findViewById(R.id.listView);
+        mAdapter = new ArrayAdapter<Address>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(mAdapter);
+
+        keywordView = (EditText) findViewById(R.id.edit_keyword);
+        Button btn = (Button) findViewById(R.id.btn_search);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String keyword = keywordView.getText().toString();
+                if (!TextUtils.isEmpty(keyword)) {
+                    if (Geocoder.isPresent()) {
+                        Geocoder geocoder = new Geocoder(MainActivity.this);
+                        try {
+                            List<Address> list = geocoder.getFromLocationName(keyword, 10);
+                            mAdapter.clear();
+                            for (Address address : list) {
+                                mAdapter.add(address);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+        });
     }
 
 
@@ -53,8 +96,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onLocationChanged(Location location) {
             if (location != null) {
-                locationView.setText("latitude : " + location.getLatitude() + ", longitude : " + location.getLongitude());
+                display(location);
             }
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mLM.removeUpdates(this);
         }
 
         @Override
@@ -83,6 +130,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission();
+            return;
+        }
+
         if (!mLM.isProviderEnabled(mProvider)) {
             if (isFirst) {
                 startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
@@ -93,17 +146,66 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        registerLocation();
+    }
+
+    private void registerLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         Location location = mLM.getLastKnownLocation(mProvider);
 
         if (location != null) {
-            locationView.setText("latitude : " + location.getLatitude() + ", longitude : " + location.getLongitude());
+            display(location);
         }
 
         mLM.requestLocationUpdates(mProvider, 0, 0, mListener);
+    }
+
+    private void display(Location location) {
+        locationView.setText("latitude : " + location.getLatitude() + ", longitude : " + location.getLongitude());
+        if (Geocoder.isPresent()) {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            try {
+                List<Address> list = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 10);
+                mAdapter.clear();
+                for (Address address : list) {
+                    mAdapter.add(address);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static final int RC_LOCATION_PERMISSION_REQUEST = 100;
+
+    private void requestLocationPermission() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) &&
+                !ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},RC_LOCATION_PERMISSION_REQUEST);
+            return;
+        }
+
+        Snackbar.make(locationView, "Location Permission....", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},RC_LOCATION_PERMISSION_REQUEST);
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != RC_LOCATION_PERMISSION_REQUEST) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            registerLocation();
+        }
     }
 
     @Override
